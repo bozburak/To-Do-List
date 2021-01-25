9102877268e9a8a3517435c92b3f368af41871d4
Burak BOZ <boz-burak@hotmail.com>
2021-01-25 18:11:30 +0300

    Update AssemblyInfo.cs

--- ToDoList/Properties/AssemblyInfo.cs
+++ ToDoList/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ToDoList")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Turkcell Iletisim Hizmetleri A.S.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ToDoList")]
-[assembly: AssemblyCopyright("Copyright © Turkcell Iletisim Hizmetleri A.S. 2020")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
